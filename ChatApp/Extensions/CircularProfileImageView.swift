93764dd15320946881b80c5b39a846350d5144b0
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2024-08-31 02:06:24 +0500

    edit- circular profile image

---
+++ ChatApp/Extensions/CircularProfileImageView.swift
@@ -0,0 +1,99 @@
+//
+//  CircularProfileImageView.swift
+//  ChatApp
+//
+//  Created by Benji Loya on 10.08.2023.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+enum ProfileImageSize {
+    case xxxLSmall
+    case xxxSmall
+    case xxSmall
+    case xSmall
+    case small
+    case medium
+    case large
+    case xLarge
+    
+    var dimension: CGFloat {
+        switch self {
+        case .xxxLSmall: return 14
+        case .xxxSmall: return 22
+        case .xxSmall: return 28
+        case .xSmall: return 34
+        case .small: return 40
+        case .medium: return 46
+        case .large: return 54
+        case .xLarge: return 60
+        }
+    }
+    
+    var fontSize: CGFloat {
+          switch self {
+          case .xxxLSmall: return 7
+          case .xxxSmall: return 11
+          case .xxSmall: return 14
+          case .xSmall: return 17
+          case .small: return 20
+          case .medium: return 23
+          case .large: return 27
+          case .xLarge: return 30
+          }
+      }
+    
+}
+
+struct UserInitialsView: View {
+    let fullname: String
+    let fontSize: CGFloat
+    
+    private var initials: String {
+        let names = fullname.split(separator: " ")
+        let firstInitial = names.first?.first?.uppercased() ?? ""
+        let lastInitial = names.count > 1 ? names.last?.first?.uppercased() ?? "" : ""
+        return "\(firstInitial)\(lastInitial)"
+    }
+    
+    var body: some View {
+        Text(initials)
+            .font(.system(size: fontSize, weight: .semibold))
+            .frame(width: fontSize * 2, height: fontSize * 2)
+            .background(Color.gray.opacity(0.25))
+            .foregroundColor(.white)
+            .clipShape(Circle())
+    }
+}
+
+// View
+struct CircularProfileImageView: View {
+    var user: User?
+    let size: ProfileImageSize
+    
+    var body: some View {
+        if let imageUrl = user?.profileImageUrl {
+            WebImage(url: URL(string: imageUrl))
+                .resizable()
+                .scaledToFill()
+                .frame(width: size.dimension, height: size.dimension)
+                .clipShape(Circle())
+                .overlay {
+                    Circle()
+                        .stroke(Color(.systemGray4), lineWidth: 0.5)
+                }
+        } else {
+            UserInitialsView(fullname: user?.fullname ?? "User", fontSize: size.fontSize)
+                .scaledToFill()
+                .frame(width: size.dimension, height: size.dimension)
+                .clipShape(Circle())
+        }
+    }
+}
+
+//struct CircularProfileImageView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CircularProfileImageView(user: dev.user, size: .large)
+//    }
+//}
