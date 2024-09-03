ebb0ad3f8bfcefb176fd04b62d10e43819722701
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2024-09-03 17:02:51 +0500

    new bunnon - sent message

--- ChatApp/Core/Chat/View/MessageInputView.swift
+++ ChatApp/Core/Chat/View/MessageInputView.swift
@@ -11,7 +11,7 @@ import PhotosUI
 struct MessageInputView: View {
     @Binding var messageText: String
     @ObservedObject var viewModel: ChatViewModel
-    
+
     var body: some View {
         ZStack(alignment: .trailing) {
             HStack {
@@ -27,7 +27,7 @@ struct MessageInputView: View {
                                 RoundedRectangle(cornerRadius: 10)
                                     .stroke(Color.gray.opacity(0.3), lineWidth: 0.5)
                             )
-                        
+
                         CustomChatButton(
                             imageName: "xmark.circle.fill",
                             font: .callout,
@@ -39,20 +39,9 @@ struct MessageInputView: View {
                         )
                     }
                     .padding(8)
-                    
+
                     Spacer()
                 } else {
-                    if messageText.isEmpty {
-                        PhotosPicker(selection: $viewModel.selectedItem) {
-                            Image(systemName: "photo.on.rectangle.angled")
-                                .font(.title3)
-                                .foregroundStyle(.gray)
-                                .padding(5)
-                                .background(Color.black.opacity(0.001))
-                                .clipShape(Circle())
-                        }
-                    }
-                    
                     TextField("Message...", text: $messageText, axis: .vertical)
                         .padding(12)
                         .padding(.leading, 4)
@@ -62,21 +51,58 @@ struct MessageInputView: View {
                         .font(.subheadline)
                 }
             }
+
+            if messageText.isEmpty {
+                PhotosPicker(selection: $viewModel.selectedItem) {
+                    Image(systemName: "photo.on.rectangle.angled")
+                        .font(.title3)
+                        .foregroundStyle(Color.theme.darkWhite.opacity(0.7))
+                        .padding(10)
+                        .background(Color.black.opacity(0.001))
+                        .clipShape(Circle())
+                }
+            } else {
+                CustomChatButton(
+                    imageName: "paperplane",
+                    font: .title3,
+                    foregroundStyle: .primary.opacity(0.7),
+                    padding: 10,
+                    onButtonPressed: {
+                        Task {
+                            try await viewModel.sendMessage(messageText)
+                            messageText = ""
+                        }
+                    }
+                )
+            }
             
-            CustomChatButton(
-                imageName: "paperplane",
-                font: .title3,
-                foregroundStyle: .primary,
-                padding: 10,
-                onButtonPressed: {
+            /*
+            Button {
+                if messageText.isEmpty {
+                  //  photo
+                } else {
                     Task {
                         try await viewModel.sendMessage(messageText)
                         messageText = ""
                     }
                 }
-            )
+            } label: {
+                MorphingSymbolView(
+                    symbol: messageText.isEmpty ? "photo" : "paperplane.fill",
+                    config: .init(
+                        font: .title2,
+                        frame: .init(width: 50, height: 50),
+                        radius: 2,
+                        foregroudColor: .primary,
+                        keyFrameDuration: 0.3,
+                        symbolAnimation: .smooth(duration: 0.3, extraBounce: 0)
+                    )
+                )
+                .background(Color.clear)
+                .clipShape(.circle)
+            }
+             */
             
-
         }
         .overlay {
             if viewModel.messageImage != nil {
@@ -95,3 +121,99 @@ struct MessageInputView_Previews: PreviewProvider {
                          viewModel: ChatViewModel(user: .mock))
     }
 }
+
+
+
+/*
+ import SwiftUI
+ import PhotosUI
+
+ struct MessageInputView: View {
+     @Binding var messageText: String
+     @ObservedObject var viewModel: ChatViewModel
+     
+     var body: some View {
+         ZStack(alignment: .trailing) {
+             HStack {
+                 if let image = viewModel.messageImage {
+                     ZStack(alignment: .topTrailing) {
+                         image
+                             .resizable()
+                             .aspectRatio(contentMode: .fill)
+                             .clipShape(Rectangle())
+                             .frame(width: 80, height: 100)
+                             .cornerRadius(10)
+                             .overlay(
+                                 RoundedRectangle(cornerRadius: 10)
+                                     .stroke(Color.gray.opacity(0.3), lineWidth: 0.5)
+                             )
+                         
+                         CustomChatButton(
+                             imageName: "xmark.circle.fill",
+                             font: .callout,
+                             foregroundStyle: .gray.opacity(0.8),
+                             padding: 5,
+                             onButtonPressed: {
+                                 viewModel.messageImage = nil
+                             }
+                         )
+                     }
+                     .padding(8)
+                     
+                     Spacer()
+                 } else {
+                     if messageText.isEmpty {
+                         PhotosPicker(selection: $viewModel.selectedItem) {
+                             Image(systemName: "photo.on.rectangle.angled")
+                                 .font(.title3)
+                                 .foregroundStyle(.gray)
+                                 .padding(5)
+                                 .background(Color.black.opacity(0.001))
+                                 .clipShape(Circle())
+                         }
+                     }
+                     
+                     TextField("Message...", text: $messageText, axis: .vertical)
+                         .padding(12)
+                         .padding(.leading, 4)
+                         .padding(.trailing, 48)
+                         .background(Color.theme.secondaryBackground)
+                         .clipShape(RoundedRectangle(cornerRadius: 20))
+                         .font(.subheadline)
+                 }
+             }
+             
+             CustomChatButton(
+                 imageName: "paperplane",
+                 font: .title3,
+                 foregroundStyle: .primary,
+                 padding: 10,
+                 onButtonPressed: {
+                     Task {
+                         try await viewModel.sendMessage(messageText)
+                         messageText = ""
+                     }
+                 }
+             )
+             
+
+         }
+         .overlay {
+             if viewModel.messageImage != nil {
+                 RoundedRectangle(cornerRadius: 10)
+                     .stroke(Color(.systemGray4), lineWidth: 0.7)
+             }
+         }
+         .padding(.horizontal)
+         .padding(.bottom, 8)
+     }
+ }
+
+ struct MessageInputView_Previews: PreviewProvider {
+     static var previews: some View {
+         MessageInputView(messageText: .constant(""),
+                          viewModel: ChatViewModel(user: .mock))
+     }
+ }
+
+ */
