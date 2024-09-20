bf460a9b41f94cd37327f072abeb48d07be0a29b
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2024-09-20 16:22:52 +0500

    update amimation notification

--- ChatApp/Core/Inbox/Components/InAppNotificationView.swift
+++ ChatApp/Core/Inbox/Components/InAppNotificationView.swift
@@ -11,10 +11,19 @@ struct InAppNotificationView: View {
     let message: Message
     let user: User
     
+    @State private var showContent: Bool = false
+    
     var body: some View {
         HStack(spacing: 10) {
             /// Иконка профиля отправителя
-            CircularProfileImageView(user: user, size: .small24)
+            if showContent {
+                CircularProfileImageView(user: user, size: .small24)
+                    .transition(.opacity)
+            } else {
+                Circle()
+                    .frame(width: 24, height: 24)
+                    .foregroundColor(.black.opacity(0.001))
+            }
             
             Text(user.username)
                 .font(.caption)
@@ -22,16 +31,23 @@ struct InAppNotificationView: View {
 
             Spacer(minLength: 0)
 
-            /// Текст сообщения или иконка чата
-            Button(action: {
-                // Можно добавить действие при нажатии, например, переход в чат с пользователем
-            }, label: {
-                Image(systemName: iconForMessageType())
-                    .font(.system(size: 16))
-                    .fontWeight(.semibold)
-                    .foregroundStyle(Color.theme.appNotification)
-                    .padding(.horizontal, 2)
-            })
+            if showContent {
+                CustomChatButton(
+                    imageName: .systemName(iconForMessageType()),
+                    font: .system(size: 16),
+                    foregroundStyle: Color.theme.appNotification,
+                    padding: 0,
+                    frame: CGSize(width: 25, height: 25),
+                    onButtonPressed: {
+                        // нажатие кнопки
+                    }
+                )
+                .transition(.opacity)
+            }  else {
+                Circle()
+                    .frame(width: 24, height: 24)
+                    .foregroundColor(.black.opacity(0.001))
+            }
         }
         .padding(.bottom, 1)
         .padding(6)
@@ -40,6 +56,18 @@ struct InAppNotificationView: View {
                 .fill(.black)
         }
         .frame(maxWidth: 200)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.53) {
+                withAnimation(.smooth(duration: 0.5)) {
+                    showContent = true
+                }
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.85) {
+                withAnimation(.smooth(duration: 0.5)) {
+                    showContent = false
+                }
+            }
+        }
     }
     
     /// Функция для выбора иконки в зависимости от типа сообщения
